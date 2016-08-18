28436759e46587ac9c0f265bb3096890a57eb8b3
v-kazaki <v-kazaki@microsoft.com>
2016-08-18 17:52:02 +0300

    14

--- runner.js
+++ runner.js
@@ -1,3 +1,4 @@
 var myTest = require("./tests/myTest");
+var browsers = ['internet explorer', 'chrome'];
 
-myTest();
+myTest(browsers);
--- tests/myTest.js
+++ tests/myTest.js
@@ -1,17 +1,19 @@
-function runTest() {
+function runTest(browsers) {
     var webdriver = require('selenium-webdriver'),
         By = webdriver.By,
         until = webdriver.until;
+        
+    browsers.forEach(function(browser) {      
+        var driver = new webdriver.Builder()
+            .forBrowser(browser) 
+            .build();
 
-    var driver = new webdriver.Builder()
-        .forBrowser('chrome')
-        .build();
-
-    driver.get('http://www.google.com/ncr');
-    driver.findElement(By.name('q')).sendKeys('webdriver');
-    driver.findElement(By.name('btnG')).click();
-    driver.wait(until.titleIs('webdriver - Google Search'), 10000);
-    driver.quit();
+        driver.get('http://www.google.com/ncr');
+        driver.findElement(By.name('q')).sendKeys('webdriver');
+        driver.findElement(By.name('btnG')).click();
+        driver.wait(until.titleIs('webdriver - Google Search'), 10000);
+        driver.quit();
+    });
 }
 
 module.exports = runTest;
