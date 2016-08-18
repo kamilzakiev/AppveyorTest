c96206d3e38b16894c3ebf453fe5da470e623cb9
v-kazaki <v-kazaki@microsoft.com>
2016-08-18 17:38:11 +0300

    11

--- tests/myTest.js
+++ tests/myTest.js
@@ -10,7 +10,7 @@ function runTest() {
     driver.get('http://www.google.com/ncr');
     driver.findElement(By.name('q')).sendKeys('webdriver');
     driver.findElement(By.name('btnG')).click();
-    driver.wait(until.titleIs('webdriver - Google Search'), 10000);
+    driver.wait(until.titleIs('webdriver - Google Search5'), 10000);
     driver.quit();
 }
 
