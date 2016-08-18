84390cba6998396b30e047fb1269b281ca925ae5
v-kazaki <v-kazaki@microsoft.com>
2016-08-18 18:02:54 +0300

    18

--- tests/myTest.js
+++ tests/myTest.js
@@ -1,6 +1,5 @@
 function runTest(browsers) {
     var webdriver = require('selenium-webdriver'),
-        By = webdriver.By,
         until = webdriver.until;
         
     browsers.forEach(function(browser) {      
@@ -9,9 +8,7 @@ function runTest(browsers) {
             .build();
 
         driver.get('http://www.google.com/ncr');
-        driver.findElement(By.name('q')).sendKeys('webdriver');
-        driver.findElement(By.name('btnG')).click();
-        driver.wait(until.titleIs('webdriver - Google Search'), 200000);
+        driver.wait(until.titleContains('Google'), 1000);
         driver.quit();
     });
 }
